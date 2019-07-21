67d25a2cb0269da3d9f6ef8114fa15393f159269
Kamil Marnik <ksdekamil@gmail.com>
2019-07-21 12:07:45 +0200

    Initial commit

---
+++ talkerr/src/main/java/com/kamilmarnik/talkerr/TalkerrApplication.java
@@ -0,0 +1,13 @@
+package com.kamilmarnik.talkerr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkerrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkerrApplication.class, args);
+    }
+
+}
