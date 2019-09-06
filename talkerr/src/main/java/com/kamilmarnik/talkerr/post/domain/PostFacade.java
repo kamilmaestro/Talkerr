1cdec76f7c7f2beb7b302b54a0efd740c0c16041
Kamil Marnik <ksdekamil@gmail.com>
2019-09-06 20:47:33 +0200

    deleting a post

--- talkerr/src/main/java/com/kamilmarnik/talkerr/post/domain/PostFacade.java
+++ talkerr/src/main/java/com/kamilmarnik/talkerr/post/domain/PostFacade.java
@@ -3,6 +3,7 @@ package com.kamilmarnik.talkerr.post.domain;
 import com.kamilmarnik.talkerr.post.dto.PostDto;
 import com.kamilmarnik.talkerr.post.exception.PostNotFoundException;
 import com.kamilmarnik.talkerr.user.domain.UserFacade;
+import com.kamilmarnik.talkerr.user.dto.UserDto;
 import com.kamilmarnik.talkerr.user.dto.UserStatusDto;
 import com.kamilmarnik.talkerr.user.exception.UserNotFoundException;
 import com.kamilmarnik.talkerr.user.exception.UserRoleException;
@@ -33,5 +34,16 @@ public class PostFacade {
     return postRepository.findById(postId)
         .orElseThrow(() -> new PostNotFoundException("Post with Id: " + postId + " was not found"))
         .dto();
+  }
+
+  public void deletePost(long postId, long userId) throws UserNotFoundException, PostNotFoundException {
+    UserDto user = userFacade.getUser(userId);
+    Objects.requireNonNull(user);
+    PostDto post = getPost(postId);
+    Objects.requireNonNull(post);
+
+    if(post.getUserId() == userId || user.getStatus().equals(UserStatusDto.ADMIN)) {
+      postRepository.deleteById(postId);
+    }
   }
 }
