0d0894b9729b665196dbd664b7d9aee7f6f5c446
Kamil Marnik <ksdekamil@gmail.com>
2019-10-22 21:26:45 +0200

    handling errors

--- talkerr/src/main/java/com/kamilmarnik/talkerr/logic/errors/ErrorResponse.java
+++ talkerr/src/main/java/com/kamilmarnik/talkerr/logic/errors/ErrorResponse.java
@@ -1,32 +1,38 @@
 package com.kamilmarnik.talkerr.logic.errors;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 class ErrorResponse {
-  private static final String MESSAGE_NOT_AVAILABLE = "Message not available!";
+  private static final String NO_MESSAGE_AVAILABLE = "No message available!";
 
   @Getter
-  int status;
+  HttpStatus status;
   String error;
   String message;
+  String errorClassName;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
   LocalDateTime date;
 
-  ErrorResponse(int status, String error, String message) {
+  ErrorResponse(HttpStatus status, String errorClassName) {
     this.status = status;
-    this.error = error;
-    this.message = message;
+    this.errorClassName = errorClassName;
+    this.message = NO_MESSAGE_AVAILABLE;
+    this.error = String.valueOf(status.value());
     this.date = LocalDateTime.now();
   }
 
-  ErrorResponse(int status, String error) {
+  ErrorResponse(HttpStatus status, String errorClassName, String message) {
     this.status = status;
-    this.error = error;
-    this.message = MESSAGE_NOT_AVAILABLE;
+    this.errorClassName = errorClassName;
+    this.message = message;
+    this.error = String.valueOf(status.value());
     this.date = LocalDateTime.now();
   }
 }
