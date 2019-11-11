c0813b19019e813c9359777169e7070e1b1aafe1
Kamil Marnik <ksdekamil@gmail.com>
2019-11-11 12:00:38 +0100

    registration without authorization

--- talkerr/src/main/java/com/kamilmarnik/talkerr/security/SecurityConfiguration.java
+++ talkerr/src/main/java/com/kamilmarnik/talkerr/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -50,5 +51,10 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
         .sessionManagement().disable()
         .csrf().disable();
+  }
+
+  @Override
+  public void configure(WebSecurity web) {
+    web.ignoring().antMatchers("/user/");
   }
 }
