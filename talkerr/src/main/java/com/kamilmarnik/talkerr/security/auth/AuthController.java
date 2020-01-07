c038194c885197aac86c9e4329ead10e7441c0dd
Kamil Marnik <ksdekamil@gmail.com>
2020-01-07 20:50:44 +0100

    mail refactor

--- talkerr/src/main/java/com/kamilmarnik/talkerr/security/auth/AuthController.java
+++ talkerr/src/main/java/com/kamilmarnik/talkerr/security/auth/AuthController.java
@@ -1,9 +1,8 @@
 package com.kamilmarnik.talkerr.security.auth;
 
-import com.kamilmarnik.talkerr.logic.Constants;
 import com.kamilmarnik.talkerr.mail.MailSender;
-import com.kamilmarnik.talkerr.user.domain.UserFacade;
 import com.kamilmarnik.talkerr.security.auth.requests.RegistrationRequest;
+import com.kamilmarnik.talkerr.user.domain.UserFacade;
 import com.kamilmarnik.talkerr.user.dto.UserDto;
 import com.kamilmarnik.talkerr.user.exception.InvalidLoginException;
 import com.kamilmarnik.talkerr.user.exception.InvalidPasswordException;
@@ -25,21 +24,18 @@ import org.springframework.web.bind.annotation.RestController;
 class AuthController {
 
   UserFacade userFacade;
+  MailSender mailSender;
 
   @Autowired
-  AuthController(@Autowired UserFacade userFacade) {
+  AuthController(@Autowired UserFacade userFacade, @Autowired MailSender mailSender) {
     this.userFacade = userFacade;
+    this.mailSender = mailSender;
   }
 
   @PostMapping("/register")
   public ResponseEntity<UserDto> registerUser(@RequestBody RegistrationRequest user) throws UserAlreadyExistsException, InvalidLoginException, InvalidPasswordException {
     UserDto registeredUser = userFacade.registerUser(user);
-    MailSender mailSender = MailSender.builder()
-        .userMail(Constants.TALKERR_MAIL)
-        .password(Constants.TALKERR_MAIL_PASSWORD)
-        .userName(user.getUsername())
-        .build();
-    mailSender.sendMail(user.getEmail());
+    mailSender.sendRegistrationConfirmationMail(user.getEmail(), user.getUsername());
 
     return ResponseEntity.ok(registeredUser);
   }
